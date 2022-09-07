5451653bd08ace6f81f914144c88d691e94fa7c0
pedablio <paulohrl96@gmail.com>
2022-09-07 02:55:56 -0300

    feat: Add typescript and new packages

--- server.ts
+++ server.ts
@@ -1,12 +1,27 @@
+import 'dotenv/config'
+
 import express from 'express'
 import compression from 'compression'
 import cors from 'cors'
 import path from 'path'
 import axios from 'axios'
 import SpotifyWebApi from 'spotify-web-api-node'
-import { Low, JSONFile } from 'lowdb'
+import JSONdb from 'simple-json-db'
 import lodash from 'lodash'
-import 'dotenv/config'
+import retry from 'retry'
+import delay from 'delay'
+
+interface Track {
+  id: string
+  albumName: string
+  name: string
+  date: string
+}
+
+interface SavedTrack {
+  name: string
+  lastTotal: number
+}
 
 const app = express()
 
@@ -15,36 +30,34 @@ app.use(compression())
 app.use(cors())
 
 const router = express.Router()
-const playlistDb = new Low(new JSONFile('playlists.json'))
+const playlistDb = new JSONdb<SavedTrack>('./playlists.json', { jsonSpaces: 2 as unknown as boolean })
 
-router.get('/', (req, res) => {
+router.get('/', (request, response) => {
   const loginUrl = 'https://accounts.spotify.com/authorize'
   const state = (Math.random() + 1).toString(36).substring(2)
   const clientId = process.env.CLIENT_ID
   const redirectUri = `${process.env.APP_URI}/callback`
   const queryUrl = `?response_type=code&client_id=${clientId}&scope=playlist-modify-public,user-library-read,user-library-modify&redirect_uri=${redirectUri}&state=${state}`
 
-  res.redirect(`${loginUrl}${queryUrl}`)
+  response.redirect(`${loginUrl}${queryUrl}`)
 })
 
-router.get('/app', (req, res) => res.sendFile(path.resolve('./app.html')))
-router.get('/playlists', (req, res) => res.sendFile(path.resolve('./playlists.json')))
-router.post('/process', async (req, res) => {
+router.get('/app', (request, response) => response.sendFile(path.resolve('./app.html')))
+router.get('/playlists', (request, response) => response.sendFile(path.resolve('./playlists.json')))
+router.post('/process', async (request, response) => {
   try {
-    const { id, lastTotal } = req.body
-    const { token } = req.query
+    const { id, lastTotal } = request.body
+    const { token } = request.query
 
-    const api = new SpotifyWebApi({ accessToken: token })
+    const api = new SpotifyWebApi({ accessToken: token as string })
     const resp = await api.getPlaylistTracks(id, { limit: 1, fields: 'total' })
 
     const { total } = resp.body
 
-    if (total === lastTotal) return res.json({ result: 'same' })
-
-    await playlistDb.read()
+    if (total === lastTotal) return response.json({ result: 'same' })
 
     const length = Math.ceil(total / 100)
-    const allTracks = []
+    const allTracks: Track[] = []
 
     for (const page of Array.from({ length }, (_, k) => k + 1)) {
       const { body } = await api.getPlaylistTracks(id, {
@@ -55,6 +68,7 @@ router.post('/process', async (req, res) => {
       allTracks.push(
         ...body.items.map(obj => ({
           id: obj.track.id,
+          albumName: obj.track.album.name,
           name: obj.track.name,
           date: obj.track.album.release_date,
         })),
@@ -62,22 +76,15 @@ router.post('/process', async (req, res) => {
     }
 
     const tracks = allTracks.map((item, number) => ({ ...item, number }))
-    const sortedTracks = lodash.orderBy(tracks, ['date', 'name'], ['desc', 'asc'])
+    const sortedTracks = lodash.orderBy(tracks, ['date', 'albumName', 'name'], ['desc', 'asc', 'asc'])
 
     let count = 0
     let changed = 0
 
     for (const track of sortedTracks) {
       if (track.number !== count) {
-        await retryWithBackOff(
-          {
-            retries: 5,
-            minDelay: 1000,
-            maxDelay: 5000,
-            factor: 2,
-          },
-          api.reorderTracksInPlaylist(id, track.number, count),
-        )
+        await delay(150)
+        await tryReorder(api, id, track.number, count)
 
         const isBefore = track.number > count
 
@@ -98,20 +105,18 @@ router.post('/process', async (req, res) => {
       count++
     }
 
-    const playlist = playlistDb.data.find(item => item.id === id)
-    playlist.lastTotal = total
+    const playlist = playlistDb.get(id)
+    playlistDb.set(id, { ...playlist, lastTotal: total })
 
-    await playlistDb.write()
-
-    return res.json({ result: 'change', tracks: changed })
+    return response.json({ result: 'change', tracks: changed })
   } catch (err) {
-    return res.json({ error: true, data: err })
+    return response.json({ error: true, data: err })
   }
 })
 
-router.post('/liked', async (req, res) => {
+router.post('/liked', async (request, response) => {
   try {
-    const { token, refresh } = req.query
+    const { token, refresh } = request.query
 
     const api = new SpotifyWebApi({
       clientId: process.env.CLIENT_ID,
@@ -119,14 +124,14 @@ router.post('/liked', async (req, res) => {
       redirectUri: process.env.APP_URI,
     })
 
-    api.setAccessToken(token)
-    api.setRefreshToken(refresh)
+    api.setAccessToken(token as string)
+    api.setRefreshToken(refresh as string)
 
     const resp = await api.getMySavedTracks({ limit: 1 })
 
     const { total } = resp.body
     const length = Math.ceil(total / 50)
-    const allTracks = []
+    const allTracks: Track[] = []
 
     for (const page of Array.from({ length }, (_, k) => k + 1)) {
       const { body } = await api.getMySavedTracks({ limit: 50, offset: (page - 1) * 50 })
@@ -146,7 +151,7 @@ router.post('/liked', async (req, res) => {
     const changedIndex = lodash.findLastIndex(sortedTracks, (track, index) => track.number !== index)
 
     if (changedIndex === -1) {
-      return res.json({ result: 'same' })
+      return response.json({ result: 'same' })
     }
 
     const changedTracks = sortedTracks.slice(0, changedIndex + 1).reverse()
@@ -158,33 +163,24 @@ router.post('/liked', async (req, res) => {
         api.setAccessToken(body.access_token)
       }
 
-      await retryWithBackOff(
-        {
-          retries: 5,
-          minDelay: 1000,
-          maxDelay: 5000,
-          factor: 2,
-        },
-        api.addToMySavedTracks([track.id]),
-      )
+      await trySave(api, track.id)
       await delay(2000)
 
       trackNumber++
     }
 
-    return res.json({ result: 'change', tracks: changedTracks.length })
+    return response.json({ result: 'change', tracks: changedTracks.length })
   } catch (err) {
-    console.log(err)
-    return res.json({ error: true, data: err })
+    return response.json({ error: true, data: err })
   }
 })
 
-router.get('/callback', async (req, res) => {
-  const code = req.query.code || null
-  const state = req.query.state || null
+router.get('/callback', async (request, response) => {
+  const code = request.query.code || null
+  const state = request.query.state || null
 
   if (state === null) {
-    return res.json({ error: true })
+    return response.json({ error: true })
   }
   try {
     const { data } = await axios.post(
@@ -193,31 +189,51 @@ router.get('/callback', async (req, res) => {
       { auth: { username: process.env.CLIENT_ID, password: process.env.CLIENT_SECRET } },
     )
 
-    return res.redirect(`${process.env.APP_URI}/app?token=${data.access_token}&refresh=${data.refresh_token}`)
+    return response.redirect(`${process.env.APP_URI}/app?token=${data.access_token}&refresh=${data.refresh_token}`)
   } catch (err) {
-    return res.json({ error: true, data: err })
+    return response.json({ error: true, data: err })
   }
 })
 
 app.use(router)
 app.listen(4354)
 
-const delay = time => new Promise(resolve => setTimeout(resolve, time))
-const calculateBackoff = ({ minDelay, maxDelay, factor }, attempt) => {
-  const attemptBackoff = minDelay * factor ** attempt
-  const backoff = Math.min(attemptBackoff, maxDelay)
-  return backoff
+async function tryReorder(api: SpotifyWebApi, playlist: string, start: number, insertBefore: number) {
+  let operation = retry.operation({ retries: 5, factor: 2 })
+
+  return new Promise<void>((resolve, reject) => {
+    operation.attempt(async currentNumber => {
+      try {
+        await api.reorderTracksInPlaylist(playlist, start, insertBefore)
+        resolve()
+      } catch (error) {
+        console.log(`Trying in ${currentNumber}`, error)
+
+        if (!operation.retry(error)) {
+          reject(operation.mainError())
+          return
+        }
+      }
+    })
+  })
 }
 
-const shouldHalt = (retries, attempt) => attempt >= retries
-const invokeAction = (config, action, args, attempt) =>
-  action(...args).catch(err =>
-    shouldHalt(config.retries, attempt)
-      ? Promise.reject(err)
-      : delay(calculateBackoff(config, attempt)).then(() => invokeAction(config, action, args, attempt + 1)),
-  )
+async function trySave(api: SpotifyWebApi, trackId: string) {
+  let operation = retry.operation({ retries: 5, factor: 2 })
 
-const retryWithBackOff =
-  (config, action) =>
-  (...args) =>
-    invokeAction(config, action, args, 0)
+  return new Promise<void>((resolve, reject) => {
+    operation.attempt(async currentNumber => {
+      try {
+        await api.addToMySavedTracks([trackId])
+        resolve()
+      } catch (error) {
+        console.log(`Trying in ${currentNumber}`, error)
+
+        if (!operation.retry(error)) {
+          reject(operation.mainError())
+          return
+        }
+      }
+    })
+  })
+}
